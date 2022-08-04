8916331414deca9f59cb5961377a7bbf5fb03568
dikhorsun <dmitry.horsun@gmail.com>
2022-08-05 00:57:50 +0300

    feat: add solving

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrBrackets = str.split('');
+  function checkArrBrackets(arrBrackets){
+    for(let i = 0; i < arrBrackets.length; i++){
+      for(let j = 0; j < bracketsConfig.length; j++){
+        if(arrBrackets[i] === bracketsConfig[j][0] && arrBrackets[i+1] === bracketsConfig[j][1]){
+          arrBrackets.splice(i, 2);
+          checkArrBrackets(arrBrackets)
+        }
+      }
+    }
+  }
+  checkArrBrackets(arrBrackets)
+  if(arrBrackets.length !== 0) return false
+  return true
 }
